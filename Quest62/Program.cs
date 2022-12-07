3c739bb3d6cca2330234e4f02973d04b07bab2b8
Handbrake brake <handbrake@mail.ru>
2022-12-08 02:41:24 +0300

    Fixes in Quest62

--- Quest62/Program.cs
+++ Quest62/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
 Console.WriteLine("Введите количество строк:");
 int rows;
 if (!int.TryParse(Console.ReadLine()!, out rows))
